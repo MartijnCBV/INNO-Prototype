44200c3771a2a01e21f022dc265c452039b19648
MartijnCBV <49876349+MartijnCBV@users.noreply.github.com>
2022-09-20 23:41:45 +0200

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <div class="border border-x-0 border-c-grey border-solid">
       <div class="mw">
         <div class="px-7">
-          <img src="src/assets/logo.svg" alt="provincie Utrecht logo" class="logo"/>
+          <img src="/src/assets/logo.svg" alt="provincie Utrecht logo" class="logo"/>
         </div>
       </div>
     </div>
